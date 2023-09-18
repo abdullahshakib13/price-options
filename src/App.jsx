dd4bc6a798a94d9198f6ce3ee4dbc63b0a4f355f
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-09-18 23:20:31 +0600

    axios data process

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 
@@ -14,6 +15,7 @@ function App() {
       
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
